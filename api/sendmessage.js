07384e3af4877c52ed5699d218c0720dde10155b
Harvey <ch4ch4@gmail.com>
2019-06-01 18:59:34 -0700

    :poop:

--- api/sendmessage.js
+++ api/sendmessage.js
@@ -1,7 +1,7 @@
-// const { parse } = require('querystring');
-const accountSid = 'ACxxxx';
-const authToken = 'xxx';
-const twilioNumber = '14154154155'
+const { parse } = require('querystring');
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_SMS_NUMBER;
 var rp = require('request-promise');
 var auth = 'Basic ' + Buffer.from(accountSid + ':' + authToken).toString('base64');
 
@@ -10,12 +10,12 @@ module.exports = (req, res) => {
     console.log(result);
     var options = {
       method: 'POST',
-      uri: 'https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json',
+      uri: `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`,
       form: {
           // Like <input type="text" name="name">
           Body: 'Hello from Next.js!',
           From: '+'+twilioNumber,
-          To: '+15305544694'
+          To: '+'+result.phone
       },
       headers: {
           /* 'content-type': 'application/x-www-form-urlencoded' */ // Is set automatically
@@ -27,27 +27,25 @@ module.exports = (req, res) => {
       .then(function (body) {
           // POST succeeded...
           // console.log(body)
-          res.end();
+          res.end("Check your phone!");
       })
       .catch(function (err) {
           // POST failed...
           res.end(JSON.stringify(err));
       });
-
-   
   });
 }
 
 function collectRequestData(request, callback) {
   const FORM_URLENCODED = 'application/x-www-form-urlencoded';
   const APPLICATION_JSON = 'application/json';
-  if(request.headers['content-type'] === APPLICATION_JSON) {
+  if(request.headers['content-type'] === FORM_URLENCODED) {
       let body = '';
       request.on('data', chunk => {
           body += chunk.toString();
       });
       request.on('end', () => {
-          callback(body);
+          callback(parse(body));
       });
   }
   else {
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import Link from 'next/link'
 
 export default function Index() {
+  function handleClick(e) {
+    e.preventDefault();
+
+    document.getElementById("phoneInput").disabled = true;
+    document.getElementById("submitBtn").disabled = true;
+    document.getElementById("workingSpan").hidden = false;
+  }
+
   return (
     <div>
       {/* <h1>Hello Next.js 👋</h1>
       <Link href='/about'><a>About</a></Link> */}
-      <h3>Enter a phone number to send a welcome message!</h3>
-      <input type="tel" placeholder="14154154155"></input>
-      <button>Send</button>
+      <h3>Enter your phone number to receive a welcome message!</h3>
+
+      <form
+          method="post"
+          action="/api/sendmessage.js">
+        <input id="phoneInput" type="tel" placeholder="14154154155" name="phone"></input>
+        <button id="submitBtn" type="submit" onClick="{handleClick}" name="submitBtn">Send</button>
+        <span id="workingSpan" hidden>  Working...</span>
+        </form>
     </div>
   )
 }
